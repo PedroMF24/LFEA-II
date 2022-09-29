014d1c0005272fb5f96b0ee1991eae543e47983b
XKronW8 <pedro.nogueira.figueiredo@gmail.com>
2022-09-29 23:10:31 +0100

    Al n3

--- Analysis.cpp
+++ Analysis.cpp
@@ -1,7 +1,7 @@
 void Analysis(){
 	//opens the file to be read
 	fstream file;
-	string filename = "Si-n2.xry";
+	string filename = "Si-n3.xry";
 	file.open(filename);
 
 	//guarantees the file is open
@@ -90,7 +90,7 @@ void Analysis(){
 	TGraph *gr = new TGraph(angle.size(), &angle[0], &rate[0]);
 	
 	gr->GetXaxis()->CenterTitle();
-	gr->SetTitle("Si n2");
+	gr->SetTitle("Si n3");
 	gr->GetXaxis()->SetTitle("Angle [#circ]");
 	gr->GetYaxis()->SetTitle("Rate [1/s]");
 	gr->SetLineColor(kBlue);
@@ -117,7 +117,7 @@ void Analysis(){
 
 	//saves the graph
 	c->Update();
-	c->SaveAs("Si-n2.png");
+	c->SaveAs("Si-n3.png");
 	//saves the fit results
 	
 }
